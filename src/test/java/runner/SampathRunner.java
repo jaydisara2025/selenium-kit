827f3c22af7d58eab7da39b684a73e1efc5936b5
Jayani Disara <jayani.disara@ifs.com>
2025-05-16 12:17:14 +0530

    Resolve merge conflict in SampathRunner.java

--- src/test/java/runner/SampathRunner.java
+++ src/test/java/runner/SampathRunner.java
@@ -1,4 +1,23 @@
 package runner;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import pages.SampathBankEX;
+
 public class SampathRunner {
+    public static void main(String[] args) {
+        // Make sure your WebDriver setup is correct for your environment
+        System.setProperty("webdriver.gecko.driver", "path/to/geckodriver");
+
+        WebDriver driver = new FirefoxDriver();
+
+        try {
+            SampathBankEX sampath = new SampathBankEX(driver);
+            sampath.sampath_bank_uk_gbp_tt_buying_rate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            driver.quit();
+        }
+    }
 }
